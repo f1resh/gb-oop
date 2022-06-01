3ea204a405c33b6c050869c1e313e32738e0e3d1
f1resh <pavel.akulov.88@gmail.com>
2022-06-01 23:05:30 +0200

    adjusting Hand class

--- BlackJack/Hand.h
+++ BlackJack/Hand.h
@@ -7,6 +7,8 @@ class Hand {
 private:
 	vector<Card*> mCards;
 public:
+	Hand();
+	virtual ~Hand();
 	void Add(Card *pCard) {
 		if (pCard == nullptr) return;
 		mCards.push_back(pCard);
@@ -14,27 +16,40 @@ public:
 	void Clear() {
 		for (auto card : mCards) {
 			delete card;
+			card = 0;
 		}
 		mCards.clear();
 	};
 	void DropHand() {
 		mCards.clear();
 	}
-	int GetTotal() {
-		int total = 0;
-		int aceNumber = 0;
-		
-		for (const auto card : mCards) {
-			total += card->GetValue();
-			if (card->rank == Rank::Ace) aceNumber++;
-		}
-
-		while (total > 21 && aceNumber > 0) {
-			total -= 10;
-			--aceNumber;
-		}
-
-		return total;
-
-	}
+	int GetTotal() const;
 };
+
+Hand::Hand() {
+	mCards.reserve(7);
+}
+
+Hand::~Hand() {
+	Clear();
+}
+
+int Hand::GetTotal() const {
+	if (mCards.empty()) return 0; // No cards in hand
+	if (mCards[0]->GetValue() == 0) return 0; // First card is face down
+	int total = 0;
+	int aceNumber = 0;
+
+	for (const auto card : mCards) {
+		total += card->GetValue();
+		if (card->rank == Rank::Ace) aceNumber++;
+	}
+
+	while (total > 21 && aceNumber > 0) {
+		total -= 10;
+		--aceNumber;
+	}
+
+	return total;
+
+}
